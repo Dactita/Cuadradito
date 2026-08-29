59ad671018882e80304487140ca712459dcedb4e
Dactita <dlcflab@gmail.com>
2026-08-29 00:01:47 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Inter:wght@400;500;600;700&display=swap"
